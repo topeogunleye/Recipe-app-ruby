093ffaad3d7fd0950de9206e77735ea8814429a9
Islam Bahnas <islambahnas1@gmail.com>
2022-07-05 16:28:02 +0200

    Update DB fix tables, Update schema

--- db/migrate/20220705125518_create_inventories.rb
+++ db/migrate/20220705125518_create_inventories.rb
@@ -1,7 +1,7 @@
 class CreateInventories < ActiveRecord::Migration[7.0]
   def change
     create_table :inventories do |t|
-      t.string name
+      t.string :name
       t.references :users, null: false, foreign_key: true
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,57 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_04_154535) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_05_134547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inventories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "users_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_inventories_on_users_id"
+  end
+
+  create_table "inventory_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "foods_id", null: false
+    t.bigint "inventories_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["foods_id"], name: "index_inventory_foods_on_foods_id"
+    t.index ["inventories_id"], name: "index_inventory_foods_on_inventories_id"
+  end
+
+  create_table "recipe_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "recipes_id", null: false
+    t.bigint "foods_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["foods_id"], name: "index_recipe_foods_on_foods_id"
+    t.index ["recipes_id"], name: "index_recipe_foods_on_recipes_id"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.integer "prepration_time"
+    t.integer "cooking_time"
+    t.string "description"
+    t.boolean "publik"
+    t.bigint "users_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_recipes_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +74,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_04_154535) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "inventories", "users", column: "users_id"
+  add_foreign_key "inventory_foods", "foods", column: "foods_id"
+  add_foreign_key "inventory_foods", "inventories", column: "inventories_id"
+  add_foreign_key "recipe_foods", "foods", column: "foods_id"
+  add_foreign_key "recipe_foods", "recipes", column: "recipes_id"
+  add_foreign_key "recipes", "users", column: "users_id"
 end
