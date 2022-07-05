02412d75fea18d1a0f1a3cf54ae8ebeb3786cae5
BonkeG <madixmicro@gmail.com>
2022-07-05 15:52:24 +0200

    add migration files

--- db/migrate/20220705130022_create_recipes.rb
+++ db/migrate/20220705130022_create_recipes.rb
@@ -1,7 +1,12 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
-
+      t.string name
+      t.integer prepration_time
+      t.integer cooking_time
+      t.string description
+      t.boolean publik
+      t.references :users, null: false, foreign_key: true
       t.timestamps
     end
   end
