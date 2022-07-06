4ba8967ac7e54adbecc87255a4194eef0938b086
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-07-06 10:44:52 +0100

    Update shopping list controllers

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,5 +1,5 @@
 class ShoppingListController < ApplicationController
   def index
-    @food = current_user.foods.includes(:recipe_foods)
+    @food = current_user.food.includes(:recipe_foods)
   end
 end
