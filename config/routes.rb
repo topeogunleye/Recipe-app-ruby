8e757fc00dc2ce68dcb3fec2dfd7d8c974e19124
Islam Bahnas <islambahnas1@gmail.com>
2022-07-06 17:16:40 +0200

    Fix sign up (try 1)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root "users#index"
+  devise_for :users
   resources :recipes
   resources :inventories
   resources :foods, only: [:index, :create, :destroy]
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
