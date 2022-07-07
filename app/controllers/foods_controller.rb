ee9a8a3528d1426255b6cf61c94a68e6f1b4d8b0
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-07-07 14:52:09 +0100

    Add foods controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,13 +1,50 @@
 class FoodsController < ApplicationController
-  def index; end
+  def index
+    @foods = Food.all
+  end
 
-  def new; end
+  def new
+    @food = Food.new
+  end
 
-  def create; end
+  def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to foods_path
+    else
+      render :new
+    end
+  end
 
-  def show; end
+  def show
+    @food = Food.find(params[:id])
+  end
 
-  def destroy; end
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path
+  end
 
-  def recipe_params; end
+  def recipe_params
+    params.require(:food).permit(:name, :description, :user_id)
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :description, :user_id)
+  end
+
+  def edit
+    @food = Food.find(params[:id])
+  end
+
+  def update
+    @food = Food.find(params[:id])
+    if @food.update(food_params)
+      redirect_to foods_path
+    else
+      render :edit
+    end
+  end
+
 end
