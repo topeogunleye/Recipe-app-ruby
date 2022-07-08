96053d454933293554df1ab4a4847e1826d335b2
BonkeG <madixmicro@gmail.com>
2022-07-08 11:08:24 +0200

    Fix merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :recipe_food, only: [:new, :create, :destroy]
   end
   resources :inventories
-  resources :foods, only: [:index, :create, :destroy]
+  resources :foods, only: [:index, :new, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
